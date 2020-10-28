244a3360c74d1022e1449090d3ef3582a745a545
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-28 07:43:21 -0700

    [Manasi]UC2 created employee payroll table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -2,3 +2,13 @@
 create database employee_payroll;
 USE employee_payroll;
 select database();
+#UC2
+create table employee_payroll
+(
+id    INT unsigned NOT NULL AUTO_INCREMENT,
+name  VARCHAR(150) NOT NULL,
+salary DOUBLE NOT NULL,
+start  DATE NOT NULL,
+primary key (id)
+);
+Describe employee_payroll;
